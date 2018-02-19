f2ca272ef940bfb44a3bc64b39fcd2ca4ad267b2
Ansuman Mohanty <oliverwayne2017@gmail.com>
2018-02-19 10:38:48 +0530

    Comments for clarity

--- Q7/main.c
+++ Q7/main.c
@@ -11,18 +11,21 @@ int main()
     for(i=0;i<n;i++)
         scanf("%d %d", &lower[i], &higher[i]);
 
-    int timeconsumed=0;
+    int timeconsumed=0; // Keeps a track of wait-time for a particular dude
     for(i=0;i<n;i++)
-    {   if(i!=0&&lower[i-1]+timeconsumed<=lower[i])
-            timeconsumed=0;
-        else if(i!=0 && lower[i-1]+timeconsumed>lower[i])
+    {   if(i!=0&&lower[i-1]+timeconsumed<=lower[i]) //If the ith dude comes after/just at time when the (i-1)th dude was handed over the sweet, then he doesn't wait.
+            timeconsumed=0;                         //Hence the wait-time for the ith dude is updated to zero for now.
+
+        else if(i!=0 && lower[i-1]+timeconsumed>lower[i])   /*This states that the ith dude has to wait for sometime. That "sometime" is
+                                                            (((i-1) dude's arrival + time waited by him till he leaves)-i dude's arrival)*/
             timeconsumed=lower[i-1]+ timeconsumed - lower[i];
-        if(lower[i]+timeconsumed<=higher[i])
+        if(lower[i]+timeconsumed<=higher[i])  /* At this point the timeconsumed variable holds the wait time for ith dude from after his arrival
+                                                till the point when the caterer starts to give him sweet. If it coincides/is less than his departure, he doesn't get sweet*/
 
         {
             //printf("timeconsumed %d\n",timeconsumed);
             printf("%d ",lower[i]+timeconsumed);
-            timeconsumed++;
+            timeconsumed++;  /*wait time incremented by 1 owing to time taken by the caterer */
 
         }
         else
